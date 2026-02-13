b212d759ecfffefcae1c292e09570f552261b7d6
Rasmus Graver <rasmus.graver@gmail.com>
2026-02-13 11:48:23 +0100

    Monty Hall JS

--- JS/MontyHall/script.js
+++ JS/MontyHall/script.js
@@ -1,1 +1,43 @@
-// JS here
+// Globale variable:
+let doorOpened = false
+
+// Finn vinnertallet:
+const vinnertall = Math.floor(Math.random() * 3)
+console.log("Vinnertallet er", vinnertall)
+
+// Add event listener (click) på hver dør:
+const allDoors = document.querySelectorAll(".door")
+console.log(allDoors)
+
+// "Ta tak i" vinnerdøren:
+const vinnerDoor = allDoors[vinnertall]
+console.log(vinnerDoor)
+
+// Bytt ut bildet til en ferrari:
+const goatImg = vinnerDoor.querySelector(".goat")
+goatImg.src = "media/ferrari.png"
+
+// Gå igjennom hver dør med en forEach:
+allDoors.forEach(door => {
+    door.addEventListener("click", klikk)
+})
+
+function klikk(event) {
+    console.log("Du klikket på en dør!")
+    // Se hva som finnes på denne eventen:
+    console.log(event)
+
+    // Åpne døren - bare dersom du ikke har åpnet noen dør enda
+    if (doorOpened == false) {
+        const door = event.target
+        door.src = "media/open.png"
+        doorOpened = true
+
+        const goatImg = door.nextElementSibling
+        if (goatImg.src.includes("ferrari")) {
+            document.body.style.backgroundColor = "green"
+        }
+    }
+}
+
+
