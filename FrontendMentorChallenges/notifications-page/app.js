a6cdf6047025dc126087e7a4de449777577b020a
Rasmus Graver <rasmus.graver@gmail.com>
2026-03-20 11:19:04 +0100

    notification page js

--- FrontendMentorChallenges/notifications-page/app.js
+++ FrontendMentorChallenges/notifications-page/app.js
@@ -1,11 +1,21 @@
-
 const allUnreads = document.querySelectorAll(".boks.unread")
-
 allUnreads.forEach(unread => {
-
     unread.addEventListener("click", function () {
-        unread.classList.remove("unread")
+        unread.classList.toggle("unread")
+        updateCounter()
     })
-
 })
 
+const markAll = document.getElementById("markall")
+markAll.addEventListener("click", function () {
+    allUnreads.forEach(unread => {
+        unread.classList.remove("unread")
+    })
+    updateCounter()
+})
+
+function updateCounter() {
+    const counterElm = document.getElementById("counter")
+    const antall = document.querySelectorAll(".boks.unread").length
+    counterElm.innerHTML = antall
+}
